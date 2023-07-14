8412e98de250d1843cf6ff614133ee66f5b04aad
thgee <102576089+thgee@users.noreply.github.com>
2023-07-15 03:35:55 +0900

    FEAT: 좌표로 주변 음식점 검색하는 함수 작성, env파일 생성
    - csv로 음식점 리스트 저장해서 처리하는 방식을 버림
    - 대신에 카카오API를 이용하여 특정 좌표 근처의 음식점을 불러옴
    - env파일 생성하여 API 키 관리함

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
 const express = require("express");
 const app = express();
 app.use("/public", express.static("public"));
+require("dotenv").config();
 
-var proj4 = require("proj4");
+console.log(
+  "===================\n===================\n=====코드실행======\n===================\n===============================================================================================\n"
+);
 
 // app.listen(8080, function () {
 //   console.log(
+
 //     "===================\n=====서버실행======\n===================\n"
 //   );
 // });
 
-const csv = require("csv-parser");
-const fs = require("fs");
-const results = [];
+// ----------------------------- 키워드로 장소 검색 ---------------------------------
 
-fs.createReadStream(__dirname + "/public/restaurant_list.csv")
-  .pipe(
-    csv({
-      mapHeaders: ({ header }) => header.trim(),
-    })
-  )
-  .on("data", (data) => results.push(data))
-  .on("end", () => {
-    // ---------------- 좌표 변환 --------------------
+// const getPos = () => {
+//   let keyword = "미식반점";
+//   fetch(
+//     `https://dapi.kakao.com/v2/local/search/keyword.json?page=1&size=15&sort=accuracy&query=${keyword}`,
+//     {
+//       method: "GET",
+//       headers: { Authorization: process.env.KAKAO_API },
+//     }
+//   )
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//     });
+// };
 
-    var epgs2097 =
-      "+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs +towgs84=-115.80,474.99,674.11,1.16,-2.31,-1.63,6.43";
-    var wgs84 =
-      "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs";
+// ----------------------------- 주변 음식점 검색 ---------------------------------
 
-    let epgs2097Pos;
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].num == 100091) {
-        epgs2097Pos = [results[i].X, results[i].Y];
-        console.log(results[i]);
-      }
+const getRestaurant = (posX, posY) => {
+  fetch(
+    `https://dapi.kakao.com/v2/local/search/category.json?category_group_code=FD6&x=${posX}&y=${posY}&radius=100`,
+    {
+      method: "GET",
+      headers: { Authorization: process.env.KAKAO_API },
     }
-    var wgs84Pos = proj4(epgs2097, wgs84, [
-      parseInt(epgs2097Pos[0]),
-      parseInt(epgs2097Pos[1]),
-    ]);
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+};
 
-    console.log(wgs84Pos[1], wgs84Pos[0]);
-  });
+getRestaurant(127.07083238839516, 37.548888556812464);
+
+// ----------------------------- 이미지 검색 ---------------------------------

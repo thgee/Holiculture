61c0d90bf77ecf303ed14666647fd4d2193e5720
thgee <102576089+thgee@users.noreply.github.com>
2023-08-21 17:46:09 +0900

    REFACTOR: 장소 검색 쿼리에서 지역명 수식어 제거

--- utils/getImgKakao.js
+++ utils/getImgKakao.js
@@ -1,9 +1,8 @@
 const getImgKakao = (place) => {
-  const dong = place.address_name.match(/(\S+)동(?=\s| >|$)/);
+  // const dong = place.address_name.match(/(\S+)동(?=\s| >|$)/);
+  // ${dong && dong[0]} 동 필요할 시 쿼리 place_name 앞에 추가할 것
   return fetch(
-    `https://dapi.kakao.com/v2/search/image?sort=accuracy&page=1&query=${
-      dong && dong[0]
-    } ${place.place_name}&size=1`,
+    `https://dapi.kakao.com/v2/search/image?sort=accuracy&page=1&query=${place.place_name}&size=1`,
     {
       method: "GET",
       headers: { Authorization: process.env.KAKAO_API },

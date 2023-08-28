64672e9c8a37f1a2440da82c4c7a90fc9de1a1e0
thgee <102576089+thgee@users.noreply.github.com>
2023-08-28 14:02:50 +0900

    FIX: 장소 조회 시 콘서트명 대신 티켓ID를 쿼리로 받음

--- routes/play.js
+++ routes/play.js
@@ -6,7 +6,7 @@ router.get("/", (req, response) => {
   let db = req.db; // server.js 에서 넘겨준 db
 
   db.collection("ticket").findOne(
-    { uuid: req.header("uuid"), concert: req.query.concert },
+    { uuid: req.header("uuid"), _id: parseInt(req.query.ticketId) },
     (err, result) => {
       if (err) return response.status(500).send("internet error");
       if (!result) return response.status(404).send("invalid ticket or uuid");
